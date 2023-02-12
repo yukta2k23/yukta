88398be40c90e207688f1f75d4925be9f9a4f89d
yukta2k23 <yukta2k23@gmail.com>
2023-02-12 21:56:48 +0530

    Intial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:yuktha2k23/front_end_files/department_list.dart';
 import 'package:yuktha2k23/front_end_files/login_page.dart';
@@ -11,11 +12,42 @@ Future<void> main() async{
   runApp(MaterialApp(
     title: 'Yukta',
     debugShowCheckedModeBanner: false,
-    home: login_page(),
+    home: login_check(),
 
     routes:{
       '/login_page' : (context) => const login_page() ,
       '/department_list' : (context) => const department_list()
     },
   ));
+}
+
+class login_check extends StatelessWidget {
+  const login_check({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context,snapshot){
+
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return const Center(child: CircularProgressIndicator(color: Colors.red,),) ;
+            }
+
+            else if(snapshot.hasError){
+              return const Center(child: Text("Access Denied"),) ;
+            }
+
+            else if(snapshot.hasData){
+              return const department_list() ;
+            }
+
+            else{
+              return const login_page() ;
+            }
+          }
+      ),
+    );
+  }
 }
